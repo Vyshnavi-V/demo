c31a6a59ff2b67916e40bfcd13d1adcdef3cb19f
lekshmi-02 <85623989+lekshmi-02@users.noreply.github.com>
2021-08-15 12:01:57 +0530

    added primenum and armstrongnum

--- calculator.cpp
+++ calculator.cpp
@@ -72,3 +72,41 @@ void binarycal()
 				printf("Error! operator is not correct");
 		}
 	}
+void primenum()
+			{
+				int n,i,m=0,flag=0;    
+				printf("Enter the number to check prime:");    
+				scanf("%d",&n);    
+				m=n/2;    
+      			for(i=2;i<=m;i++)    
+				{    
+					if(n%i==0)    
+						{    
+							printf("Number is not prime");    
+							flag=1;    
+							break;    
+						}    
+
+					if(flag==0)
+						printf("Number is prime");
+				}
+			}
+
+void armstrongnum()
+
+	{
+		int n,r, sum=0,temp;
+		printf("Enter the number to find Armstrong or not :" );
+		scanf("%d",&n);
+		temp=n;
+		while(n>0)
+			{
+					r=n%10;
+					sum=sum+(r*r*r);
+					n=n/10;
+			}
+		if(temp==sum)
+			printf("armstrong  number ");
+		else
+			printf("not armstrong number");
+	}
